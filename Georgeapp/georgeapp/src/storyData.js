71b151d6ecfd47ea2ec4c10ebd4c211ab1bc1465
JoshJoseph0 <JOSH.JOSEPH24@bathspa.ac.uk>
2026-05-19 17:39:45 +0100

    new

--- Georgeapp/georgeapp/src/storyData.js
+++ Georgeapp/georgeapp/src/storyData.js
@@ -12,7 +12,7 @@ export const CHAPTERS = [
         title: "Hi there, my name is George.",
         body:
           "Hi there, my name is George. Well, that's what the museum likes to call me. I'm not really sure who made me, but I've been around for a hundred years, so I've seen a lot of amazing things.",
-        audio: '/audio/ch1-s1.wav',
+        audio: '/audio/First.mp3',
         isInteraction: false,
       },
       {
@@ -20,7 +20,7 @@ export const CHAPTERS = [
         title: 'A Bit Old Now',
         body:
           "I might look a bit old now and I've lost a few fingers and the rest are quite wobbly, but that's just because I've had lots of adventures. Despite how old I'm getting, it's still lovely being here for you all today.",
-        audio: '/audio/ch1-s2.wav',
+        audio: '/audio/2.mp3',
         isInteraction: false,
       },
     ],
@@ -38,7 +38,7 @@ export const CHAPTERS = [
         title: 'Meeting Walter Sickert',
         body:
           "I spent most of my younger years with a kind man called Walter Sickert and he took really good care of me. I even got to help him when he was making some of his paintings. How cool is that? It does make me feel a little shy, but also very proud of what I've helped to accomplish.",
-        audio: '/audio/ch2-s1.wav',
+        audio: '/audio/3.mp3',
         isInteraction: false,
       },
     ],
@@ -56,7 +56,7 @@ export const CHAPTERS = [
         title: 'Education at Bath Spa',
         body:
           "When he passed, he sent me to the Bath School of Art. I was looking forward to helping people learn, although I didn't get a degree myself. And after a few trips to Europe, I ended up in the Bath Spa University collection.",
-        audio: '/audio/ch3-s1.wav',
+        audio: '/audio/4.mp3',
         isInteraction: false,
       },
     ],
@@ -74,7 +74,7 @@ export const CHAPTERS = [
         title: 'Lazarus Paintings',
         body:
           "I used to be a lot more flexible and helpful for Walter when he was working on the Lazarus paintings, especially since I could hold my poses forever.",
-        audio: '/audio/ch4-s1.wav',
+        audio: '/audio/5.mp3',
         isInteraction: false,
       },
     ],
@@ -92,7 +92,7 @@ export const CHAPTERS = [
         title: 'The Thrill of an Audience',
         body:
           "Thank you everyone for letting me chat a little bit about my memories. I really could get used to audiences like this. It's rather thrilling.",
-        audio: '/audio/ch5-s1.wav',
+        audio: '/audio/6.mp3',
         isInteraction: false,
       },
     ],
@@ -109,7 +109,7 @@ export const CHAPTERS = [
         id: 'q-s1',
         title: 'Question One',
         body: '',
-        audio: '/audio/q1.wav',
+        audio: '/audio/Q1.mp3',
         isInteraction: true,
         interactionPrompt:
           'How do you think I lost these fingers?',
@@ -119,7 +119,7 @@ export const CHAPTERS = [
         id: 'q-s2',
         title: 'Question Two',
         body: '',
-        audio: '/audio/q2.wav',
+        audio: '/audio/Q2.mp3',
         isInteraction: true,
         interactionPrompt:
           'How do you think I got this lipstick on my face? The truth is, I have no idea how this ended up here, and neither does the university. I\'d bet it was one of those mischievous students at the Bath School of Art!',
@@ -129,7 +129,7 @@ export const CHAPTERS = [
         id: 'q-s3',
         title: 'Question Three',
         body: '',
-        audio: '/audio/q3.wav',
+        audio: '/audio/Q3.mp3',
         isInteraction: true,
         interactionPrompt:
           'What inspired Walter to use mannequins like myself as inspiration for his artwork?',
@@ -140,7 +140,7 @@ export const CHAPTERS = [
         title: "Time's Up!",
         body:
           "His use of wooden mannequins came from a desire to create believability and scale in his work.",
-        audio: '/audio/q3-answer.wav',
+        audio: '/audio/Times up.mp3',
         isInteraction: false,
       },
     ],
